1bda4b9b3852648eb9b549918a1c86bbc57b6d0b
MelGeorge <mmgeorg@umich.edu>
2017-07-03 14:07:57 -0400

    Updated contacts tab, added current tab highlighting

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 ## Welcome to MelGeorge.github.io
 
-This project is meant to be an interactive portfolio/ resume, developed using html/css, jQuery, and animate.css (see https://daneden.github.io/animate.css/ ). 
+This project is meant to be an interactive portfolio/ resume, developed using html/css and jQuery. 
+
+Credits:
+
+Some animations from:
+https://daneden.github.io/animate.css/
+
+Some "contact" tab images from:
+http://www.mediafire.com/file/iawyvrmuenadno9/Services-Images.zip
+
+Official logos from:
+GitHub.com
+LinkedIn.com
+
+A resource I have found useful -- videos by Tom Walsh:
+https://www.youtube.com/channel/UCkRNZ2HAVO6YGLCXY58n5TQ
 
 In progress!
--- animations.js
+++ animations.js
@@ -1,22 +1,44 @@
 $(document).ready(function(){
-    $("div").hide();
+    $(".about").hide();
+    $(".exp").hide();
+    $(".proj").hide();
+    $(".edu").hide();
+    $(".cont").hide();
     $(".logo").show();
+    $("#home").addClass("currentTab");
         
     $("#home, #aboutMe, #experience, #education, #projects, #contact").click(function(){
-        $("div").fadeOut(300).delay(500);
+        $(".logo").fadeOut(300).delay(500);
+        $("#home").removeClass("currentTab");
+        $(".about").fadeOut(300).delay(500);
+        $("#aboutMe").removeClass("currentTab");
+        $(".exp").fadeOut(300).delay(500);
+        $("#experience").removeClass("currentTab");
+        $(".proj").fadeOut(300).delay(500);
+        $("#projects").removeClass("currentTab");
+        $(".edu").fadeOut(300).delay(500);
+        $("#education").removeClass("currentTab");
+        $(".cont").fadeOut(300).delay(500);
+        $("#contact").removeClass("currentTab");
                    
         if(this.id == "home") {
             $(".logo").fadeIn(300);
+            $("#home").addClass("currentTab");
         } else if(this.id == "aboutMe") {
             $(".about").fadeIn(300);
+            $("#aboutMe").addClass("currentTab");
         } else if(this.id == "experience") {
             $(".exp").fadeIn(300);
+            $("#experience").addClass("currentTab");
         } else if(this.id == "education") {
             $(".edu").fadeIn(300);
+            $("#education").addClass("currentTab");
         } else if(this.id == "projects") {
             $(".proj").fadeIn(300);
+            $("#projects").addClass("currentTab");
         } else if(this.id == "contact") {
             $(".cont").fadeIn(300);
+            $("#contact").addClass("currentTab");
         }
     });
 });
