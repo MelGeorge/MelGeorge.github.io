f796205267d560f244e1a356f56b89e6ec8a3e4b
MelGeorge <mmgeorg@umich.edu>
2017-07-04 22:05:41 -0400

    Updated About Me section

--- animations.js
+++ animations.js
@@ -8,36 +8,36 @@ $(document).ready(function(){
     $("#home").addClass("currentTab");
         
     $("#home, #aboutMe, #experience, #education, #projects, #contact").click(function(){
-        $(".logo").fadeOut(300).delay(500);
+        $(".logo").fadeOut(500).delay(500);
         $("#home").removeClass("currentTab");
-        $(".about").fadeOut(300).delay(500);
+        $(".about").fadeOut(500).delay(500);
         $("#aboutMe").removeClass("currentTab");
-        $(".exp").fadeOut(300).delay(500);
+        $(".exp").fadeOut(500).delay(500);
         $("#experience").removeClass("currentTab");
-        $(".proj").fadeOut(300).delay(500);
+        $(".proj").fadeOut(500).delay(500);
         $("#projects").removeClass("currentTab");
-        $(".edu").fadeOut(300).delay(500);
+        $(".edu").fadeOut(500).delay(500);
         $("#education").removeClass("currentTab");
-        $(".cont").fadeOut(300).delay(500);
+        $(".cont").fadeOut(500).delay(500);
         $("#contact").removeClass("currentTab");
                    
         if(this.id == "home") {
-            $(".logo").fadeIn(300);
+            $(".logo").fadeIn(500);
             $("#home").addClass("currentTab");
         } else if(this.id == "aboutMe") {
-            $(".about").fadeIn(300);
+            $(".about").fadeIn(500);
             $("#aboutMe").addClass("currentTab");
         } else if(this.id == "experience") {
-            $(".exp").fadeIn(300);
+            $(".exp").fadeIn(500);
             $("#experience").addClass("currentTab");
         } else if(this.id == "education") {
-            $(".edu").fadeIn(300);
+            $(".edu").fadeIn(500);
             $("#education").addClass("currentTab");
         } else if(this.id == "projects") {
-            $(".proj").fadeIn(300);
+            $(".proj").fadeIn(500);
             $("#projects").addClass("currentTab");
         } else if(this.id == "contact") {
-            $(".cont").fadeIn(300);
+            $(".cont").fadeIn(500);
             $("#contact").addClass("currentTab");
         }
     });
